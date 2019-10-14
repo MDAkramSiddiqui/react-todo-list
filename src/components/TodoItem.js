2c80eec2d8e9c846c69980dfb2d57bbf0511effe
MDAkram077 <40524561+MDAkram077@users.noreply.github.com>
2019-10-14 17:44:12 +0530

    First Commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <div>
+                Hello I am an Item
+            </div>
+        )
+    }
+}
