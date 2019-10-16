de47cf8cb75858854f32aa949a5bc94dae94b442
MDAkram077 <40524561+MDAkram077@users.noreply.github.com>
2019-10-16 11:37:25 +0530

    App Building Start

--- src/App.js
+++ src/App.js
@@ -6,13 +6,61 @@ import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 
 export default class App extends Component {
+
+  state = {
+    items: [
+      {id: 1, title: "wake up"},
+      {id: 2, title: "eat breakfast"}
+    ],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+
+  handleChange = (event) => {
+    console.log('Handle Change');
+  };
+
+  handleSubmit = (event) => {
+    console.log('Handle Submit');
+  };
+
+  clearList = (event) => {
+    console.log('Clear List');
+  };
+
+  handleDelete = (event) => {
+    console.log('Handle Delete');
+  };
+
+  handleEdit = (event) => {
+    console.log('Handle Edit');
+  };
+
   render() {
     return (
       <div>
         <div className="container">
           <div className="row">
-            <TodoInput />
-            <TodoList />
+            <div className="col-10 mx-auto col-md-8 mt-5">
+
+              <h3 className="text-capitalize text-center">todo input</h3>
+
+              <TodoInput 
+              item={this.state.item} 
+              handleSubmit={this.handleSubmit} 
+              handleChange={this.handleChange} 
+              editItem={this.state.editItem} 
+              />
+
+              <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleEdit={this.handleEdit}
+              handleDelete={this.handleDelete}
+              />
+
+            </div>
           </div>
         </div>
       </div>
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
     render() {
+        const { item, handleSubmit, handleChange, editItem } = this.props;
+
         return (
-            <section>
-                <h1>Todo Input</h1>
-            </section>
+            <div className="card card-body my-3">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text bg-primary text-white">
+                                <i class="fas fa-book"></i>
+                            </div>
+                        </div>
+
+                        <input 
+                        type="text" 
+                        className="form-control text-capitalize" 
+                        placeholder="add todo item"
+                        value={item}
+                        onChange={handleChange}
+                        style={{borderRadius: "3px"}}
+                        />
+
+                        <button className="btn btn-block btn-primary text-uppercase mt-3" type="Submit">add item</button>
+
+                    </div>
+                </form>
+            </div>
         )
     }
 }
